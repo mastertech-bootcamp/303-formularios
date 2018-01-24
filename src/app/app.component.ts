c6f1c2314b872521399d6640eefcce724bde355e
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-24 13:29:56 -0200

    Uso do FormsModule e ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  menuVisivel = false;
+  dados = {titulo: '', conteudo: ''};
+
+  alternarMenu(){
+    if(this.menuVisivel){
+      this.menuVisivel = false;
+    }else{
+      this.menuVisivel = true;
+    }
+  }
 }
